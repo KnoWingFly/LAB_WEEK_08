5570e6c61a87942f61482e77302609670ef4f3bd
KnowingFly <UnKnownOwl26@gmail.com>
2025-10-29 16:16:38 +0700

    Commit No. 3 – add third worker & second foreground service

--- app/src/main/java/com/example/lab_week_08/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_08/MainActivity.kt
@@ -1,105 +1,136 @@
-package com.example.lab_week_08
+    package com.example.lab_week_08
 
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
-import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.work.Constraints
-import androidx.work.Data
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
-import com.example.lab_week_08.service.NotificationService
-import com.example.lab_week_08.worker.FirstWorker
-import com.example.lab_week_08.worker.SecondWorker
+    import android.content.Intent
+    import android.content.pm.PackageManager
+    import android.os.Build
+    import android.os.Bundle
+    import android.widget.Toast
+    import androidx.activity.enableEdgeToEdge
+    import androidx.appcompat.app.AppCompatActivity
+    import androidx.core.content.ContextCompat
+    import androidx.core.view.ViewCompat
+    import androidx.core.view.WindowInsetsCompat
+    import androidx.work.Constraints
+    import androidx.work.Data
+    import androidx.work.NetworkType
+    import androidx.work.OneTimeWorkRequest
+    import androidx.work.WorkManager
+    import com.example.lab_week_08.service.NotificationService
+    import com.example.lab_week_08.service.SecondNotificationService
+    import com.example.lab_week_08.worker.FirstWorker
+    import com.example.lab_week_08.worker.SecondWorker
+    import com.example.lab_week_08.worker.ThirdWorker
 
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
 
-    private val workManager = WorkManager.getInstance(this)
+        private val workManager = WorkManager.getInstance(this)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
-            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED){
-                requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            enableEdgeToEdge()
+            setContentView(R.layout.activity_main)
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
             }
-        }
-
-        val networkConstraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.CONNECTED)
-            .build()
-
-        val id = "001"
-
-        val firstRequest = OneTimeWorkRequest
-            .Builder(FirstWorker::class.java)
-            .setConstraints(networkConstraints)
-            .setInputData(getIdInputData(FirstWorker.INPUT_DATA_ID, id))
-            .build()
-
-        val secondRequest = OneTimeWorkRequest
-            .Builder(SecondWorker::class.java)
-            .setConstraints(networkConstraints)
-            .setInputData(getIdInputData(SecondWorker.INPUT_DATA_ID, id))
-            .build()
-
-        workManager.beginWith(firstRequest)
-            .then(secondRequest)
-            .enqueue()
-
-        workManager.getWorkInfoByIdLiveData(firstRequest.id)
-            .observe(this){info ->
-                if (info != null && info.state.isFinished) {
-                    showResult("First Process is Done")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED){
+                    requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
                 }
             }
-        workManager.getWorkInfoByIdLiveData(secondRequest.id)
-            .observe(this){info ->
-                if (info != null && info.state.isFinished) {
-                    showResult("Second Process is Done")
-                    launchNotificationService()
+
+            val networkConstraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build()
+
+            val id = "001"
+
+            val firstRequest = OneTimeWorkRequest
+                .Builder(FirstWorker::class.java)
+                .setConstraints(networkConstraints)
+                .setInputData(getIdInputData(FirstWorker.INPUT_DATA_ID, id))
+                .build()
+
+            val secondRequest = OneTimeWorkRequest
+                .Builder(SecondWorker::class.java)
+                .setConstraints(networkConstraints)
+                .setInputData(getIdInputData(SecondWorker.INPUT_DATA_ID, id))
+                .build()
+
+            val thirdRequest = OneTimeWorkRequest
+                .Builder(ThirdWorker::class.java)
+                .setConstraints(networkConstraints)
+                .setInputData(getIdInputData(ThirdWorker.INPUT_DATA_ID, id))
+                .build()
+
+            workManager.beginWith(firstRequest)
+                .then(secondRequest)
+                .enqueue()
+
+            workManager.getWorkInfoByIdLiveData(firstRequest.id)
+                .observe(this) { info ->
+                    if (info != null && info.state.isFinished) {
+                        showResult("First Process is Done")
+                    }
                 }
+
+            workManager.getWorkInfoByIdLiveData(secondRequest.id)
+                .observe(this) { info ->
+                    if (info != null && info.state.isFinished) {
+                        showResult("Second Process is Done")
+                        launchNotificationService()
+                    }
+                }
+
+            NotificationService.trackingCompletion.observe(this) { id ->
+                showResult("Process for Notification Channel ID $id is done!")
+                workManager.enqueue(thirdRequest)
             }
-    }
 
-    private fun getIdInputData(idKey: String, idValue: String) =
-        Data.Builder()
-            .putString(idKey, idValue)
-            .build()
+            workManager.getWorkInfoByIdLiveData(thirdRequest.id)
+                .observe(this) { info ->
+                    if (info != null && info.state.isFinished) {
+                        showResult("Third Process is Done")
+                        launchSecondNotificationService()
+                    }
+                }
 
-    private fun showResult(message: String){
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun launchNotificationService(){
-        NotificationService.trackingCompletion.observe(this){
-            id ->
-            showResult("Process for Notification Channel ID $id is Done!")
+            SecondNotificationService.trackingCompletion.observe(this) { id ->
+                showResult("Process for Second Notification Channel ID $id is done!")
+            }
         }
 
-        val serviceIntent = Intent(this,
-            NotificationService::class.java
-        ).apply{
-                putExtra(EXTRA_ID, "001")
+        private fun getIdInputData(idKey: String, idValue: String) =
+            Data.Builder()
+                .putString(idKey, idValue)
+                .build()
+
+        private fun showResult(message: String){
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
         }
 
-        ContextCompat.startForegroundService(this,serviceIntent)
-    }
+        private fun launchNotificationService(){
+            val serviceIntent = Intent(this,
+                NotificationService::class.java
+            ).apply{
+                    putExtra(EXTRA_ID, "001")
+            }
 
-    companion object{
-        const val EXTRA_ID = "id"
+            ContextCompat.startForegroundService(this,serviceIntent)
+        }
+
+        private fun launchSecondNotificationService(){
+            val serviceIntent = Intent(this,
+                SecondNotificationService::class.java
+            ).apply{
+                putExtra(EXTRA_ID, "002")
+            }
+
+            ContextCompat.startForegroundService(this,serviceIntent)
+        }
+
+        companion object{
+            const val EXTRA_ID = "id"
+        }
     }
-}
